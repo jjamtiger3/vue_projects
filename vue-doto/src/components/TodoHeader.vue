338c3fa9f3b2bd49e7071eab3ee5834ac9e675fc
seois830926 <idmahoro@gmail.com>
2019-05-01 11:16:38 +0900

    TODO list 삭제 중간커밋

---
+++ vue-doto/src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <div>
+      <h1>Todo it!!!!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+  h1 {
+    color: #2f3b52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
